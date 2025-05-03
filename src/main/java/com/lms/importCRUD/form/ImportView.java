0cdb2323fc854b653abc83c745c754d5123f2bfb
Vinh9122 <nvphuongvinh@gmail.com>
2025-05-03 19:54:38 +0700

    Implement basic statistics: total books, books by category, total readers, readers by gender, borrowed books, and overdue readers

---
+++ src/main/java/com/lms/importCRUD/form/ImportView.java
@@ -0,0 +1,26 @@
+package com.lms.importCRUD.form;
+
+import java.awt.CardLayout;
+
+import javax.swing.*;
+
+import com.lms.auth.entities.User;
+import com.lms.importCRUD.form.other.ImportPanel;
+
+public class ImportView extends JPanel {
+    ImportPanel importPanel;
+    private CardLayout cardLayout;
+
+    public ImportView(User user) {
+        super();
+
+        importPanel = new ImportPanel(user);
+        cardLayout = new CardLayout();
+        this.setLayout(cardLayout);
+
+        this.add(importPanel, "importPanel");
+
+        cardLayout.show(this, "importPanel");
+
+    }
+}
